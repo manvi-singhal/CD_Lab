275998c87f547e7bc4bef9654795a47121b9d75c
Manvi Singhal <manvi.singhal03@gmail.com>
2024-05-15 16:32:28 +0530

    :art: fix exp 4

--- 4. Left Factoring/leftfactoring.cpp
+++ 4. Left Factoring/leftfactoring.cpp
@@ -3,36 +3,39 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     char parentNonTerminal;
     string gram, part1, part2, modifiedGram, newGram;
-    
+
     cout << "Enter Parent Non-Terminal: ";
     cin >> parentNonTerminal;
     cin.ignore();
-    
+
     cout << "Enter Production for " << parentNonTerminal << " -> ";
     getline(cin, gram);
-    
+
     size_t pos = gram.find('|');
     part1 = gram.substr(0, pos);
     part2 = gram.substr(pos + 1);
-    
+
     size_t i = 0, j = 0, k = 0;
-    for (i = 0; i < part1.length() && i < part2.length(); i++) {
-        if (part1[i] == part2[i]) {
+    for (i = 0; i < part1.length() && i < part2.length(); i++)
+    {
+        if (part1[i] == part2[i])
+        {
             modifiedGram += part1[i];
             k++;
             pos = i + 1;
         }
     }
     modifiedGram += 'X';
-    
+
     newGram = part1.substr(pos) + '|' + part2.substr(pos);
-    
+
     cout << "\nGrammar Without Left Factoring:\n";
     cout << parentNonTerminal << "->" << modifiedGram << endl;
     cout << "X->" << newGram << endl;
-    
+
     return 0;
 }
--- 4. Left Factoring/leftfactoring2.cpp
+++ 4. Left Factoring/leftfactoring2.cpp
@@ -2,45 +2,52 @@
 #include <cstring>
 using namespace std;
 
-int main() {
+int main()
+{
     char ch, lhs[20][20], rhs[20][20][20], temp[20], temp1[20];
     int n, n1, count[20], x, y, i, j, k, c[20];
 
     cout << "\nEnter the no. of nonterminals : ";
     cin >> n;
     n1 = n;
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         cout << "\nNonterminal " << i + 1 << "\nEnter the no. of productions : ";
         cin >> c[i];
         cout << "\nEnter LHS : ";
         cin >> lhs[i];
-        
-        for(j = 0; j < c[i]; j++) {
+
+        for (j = 0; j < c[i]; j++)
+        {
             cout << lhs[i] << "->";
             cin >> rhs[i][j];
         }
     }
 
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         count[i] = 1;
-        while(memcmp(rhs[i][0], rhs[i][1], count[i]) == 0)
+        while (memcmp(rhs[i][0], rhs[i][1], count[i]) == 0)
             count[i]++;
     }
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         count[i]--;
-        if(count[i] > 0) {
+        if (count[i] > 0)
+        {
             strcpy(lhs[n1], lhs[i]);
             strcat(lhs[i], "'");
-            
-            for(k = 0; k < count[i]; k++)
+
+            for (k = 0; k < count[i]; k++)
                 temp1[k] = rhs[i][0][k];
             temp1[k++] = '\0';
-            
-            for(j = 0; j < c[i]; j++) {
-                for(k = count[i], x = 0; k < strlen(rhs[i][j]); x++, k++)
+
+            for (j = 0; j < c[i]; j++)
+            {
+                for (k = count[i], x = 0; k < strlen(rhs[i][j]); x++, k++)
                     temp[x] = rhs[i][j][k];
                 temp[x++] = '\0';
-                if(strlen(rhs[i][j]) == 1)
+                if (strlen(rhs[i][j]) == 1)
                     strcpy(rhs[n1][1], rhs[i][j]);
                 strcpy(rhs[i][j], temp);
             }
@@ -52,14 +59,16 @@ int main() {
         }
     }
     cout << "\n\nThe resulting productions are : \n";
-    for(i = 0; i < n1; i++) {
-        if(i == 0)
+    for (i = 0; i < n1; i++)
+    {
+        if (i == 0)
             cout << "\n " << lhs[i] << " -> " << (char)238 << "|";
         else
             cout << "\n " << lhs[i] << " -> ";
-        for(j = 0; j < c[i]; j++) {
+        for (j = 0; j < c[i]; j++)
+        {
             cout << " " << rhs[i][j] << " ";
-            if((j + 1) != c[i])
+            if ((j + 1) != c[i])
                 cout << "|";
         }
         cout << "\b\b\b\n";
